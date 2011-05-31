679908b45865e6bea4b0e66aeb0169e92b1bfa4e
Mark Cotton <tifosigoon@gmail.com>
2011-05-30 22:25:26 -0500

    added config.js settings to git

---
+++ config.js
@@ -0,0 +1,11 @@
+// Protecting settings from github
+// through the almighty config module
+
+exports.settings = {
+    user: 'user',
+    pass: 'user',
+    host: 'localhost',
+    port: 5984, 
+    testing_db: 'testing', 
+    db: 'blog'
+};
