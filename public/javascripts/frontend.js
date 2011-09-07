7ccf341c048bf1f4bf3515ee8a9ddec1162a9995
Mark Cotton <tifosigoon@gmail.com>
2011-09-06 21:39:01 -0500

    minor changes to clean-up javascript errors

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -9,11 +9,14 @@ $(document).ready(function(){
     //items.push('<tr><td>' + data.rows[i].key + '</td><td>' + data.rows[i].value + '</td></tr>');
     items.push({"text": data.rows[i].key, "weight": data.rows[i].value});
   });
-
-  $("#tagcloud").jQCloud(items);
- 
-  });
     
+    // Only show the tag cloud on the main page
+    if ($("#tagcloud").length)  {
+      $("#tagcloud").jQCloud(items); 
+    }
+  
+  });
+  
   $('#add_post_button').click(function() {
     // This is just loading the page that
     // contains the form
@@ -24,6 +27,9 @@ $(document).ready(function(){
   $("#content li").live({
     click: function () { 
       $(this).addClass("done");
+      // Get rid of annoying hilite class
+      $(this).removeClass("hilite");
+      
       var content = $('#content').html();
       var title = $('#title').html();
       //console.log(foo);
@@ -44,7 +50,7 @@ $(document).ready(function(){
   $("#edit_submit").click(function() {
     //alert($('#content').val());
     var content = $('#content').val();
-    $.post(window.location.pathname + '/update', { content: content });  
+    $.post(window.location.pathname + 'update', { content: content });  
   
   });
 
