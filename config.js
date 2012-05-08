0d1598d78bf956e964a20ffc5bf5dc19aeaae138
Mark Cotton <tifosigoon@gmail.com>
2012-05-07 22:00:15 -0500

    Getting it updated to node v0.6.17

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // through the almighty config module
 
 exports.settings = {
-    user: 'user',
-    pass: 'user',
+    user: 'admin',
+    pass: 'admin',
     host: 'localhost',
     port: 5984, 
     testing_db: 'testing', 
